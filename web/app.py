a078946b2100ec328c1ac90ddd3f2e105708aa0b
nokiwok <91245622+nokiwok@users.noreply.github.com>
2021-10-01 10:47:46 +0900

    Update app.py

--- web/app.py
+++ web/app.py
@@ -9,7 +9,7 @@ def mainmenu():
     return """
     <html>
     <body>
-    <center><h1>Hello World! from AWS CodeDeploy.</h1><br/>
+    <center><h1>Hello World! from AWS CodeDeploy. Edit Test</h1><br/>
     </body>
     </html>"""
 
